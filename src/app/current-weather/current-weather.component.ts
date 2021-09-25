25b5f924746c1c650bf1066ca02961f6c22882d4
Guneet Rekhi <rekhi.guneet@gmail.com>
2021-09-25 13:34:09 -0700

    defining models

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../icurrent-weather';
+
 
 @Component({
   selector: 'app-current-weather',
@@ -6,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-weather.component.scss']
 })
 export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather
+  constructor() { //inject dummy data for now
+    this.current = {
+      city: 'Redmond',
+      country: 'US',
+      date: new Date(),
+      image: '',
+      temperature: 73,
+      description: 'Sunny'
+    }
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
